d4f61fbc1232e9411c255a36729946f130784486
lethanhminh <21520063@gm.uit.edu.vn>
2024-06-17 19:53:58 +0700

    add choosing number sampling and masking function for json file

---
+++ datasets/coco_split_file/masking.py
@@ -0,0 +1,49 @@
+import json
+import random 
+
+def masking_json(input_json, output_json, categories, num_images= 15000):
+    with open(input_json, 'r') as f:
+        data = json.load(f)
+
+    # Create a mapping from category id to category name
+    category_name_to_id = {cat['name']: cat['id'] for cat in data['categories']}
+    category_id_to_name = {cat['id']: cat['name'] for cat in data['categories']}
+
+    # Filter out category IDs based on the provided category names
+    selected_category_ids = set(category_name_to_id[cat] for cat in categories if cat in category_name_to_id)
+
+    structure = {
+        'info': data.get('info', {}),
+        'licenses': data.get('licenses', []),
+        'images': [],
+        'annotations': [],
+        'categories': data['categories'] + [{'id': max(category_name_to_id.values()) + 1, 'name': 'unknown'}]  # Add 'unknown' category
+    }
+
+    unknown_category_id = max(category_name_to_id.values()) + 1
+
+    # Track image ids that should be included
+    image_ids = set()
+
+    # Randomly select the specified number of image ids
+    if len(image_ids) > num_images:
+        image_ids = set(random.sample(image_ids, num_images))
+
+    # Collect image ids that have at least one annotation in the selected categories
+    for ann in data['annotations']:
+        if ann['category_id'] in selected_category_ids:
+            image_ids.add(ann['image_id'])
+
+    # Filter annotations based on collected image ids and change unspecified categories to 'unknown'
+    for ann in data['annotations']:
+        if ann['image_id'] in image_ids:
+            if ann['category_id'] not in selected_category_ids:
+                ann['category_id'] = unknown_category_id
+            structure['annotations'].append(ann)
+
+    # Filter images based on collected image ids
+    structure['images'] = [img for img in data['images'] if img['id'] in image_ids]
+
+    # Write the new JSON data to the output file
+    with open(output_json, 'w') as f:
+        json.dump(structure, f, indent=4)
--- datasets/coco_split_file/test.py
+++ datasets/coco_split_file/test.py
@@ -1,16 +1,37 @@
 import csv
-input_file = 'total_images.csv'
+import split as splt
+import test as t
+import masking as mk
 
-# Initialize a variable to store the total number of images
-total_images = 0
+T1_COCO_CLASS_NAMES = [
+    "airplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
+    "chair", "cow", "dining table", "dog", "horse", "motorcycle", "person",
+    "potted plant", "sheep", "couch", "train", "tv"
+]
+T2_CLASS_NAMES = [
+    "truck", "traffic light", "fire hydrant", "stop sign", "parking meter",
+    "bench", "elephant", "bear", "zebra", "giraffe",
+    "backpack", "umbrella", "handbag", "tie", "suitcase",
+    "microwave", "oven", "toaster", "sink", "refrigerator"
+]
+T3_CLASS_NAMES = [
+    "frisbee", "skis", "snowboard", "sports ball", "kite",
+    "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "banana", "apple", "sandwich", "orange", "broccoli",
+    "carrot", "hot dog", "pizza", "donut", "cake"
+]
+T4_CLASS_NAMES = [
+    "bed", "toilet", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "book", "clock",
+    "vase", "scissors", "teddy bear", "hair drier", "toothbrush",
+    "wine glass", "cup", "fork", "knife", "spoon", "bowl"
+]
 
-# Read the CSV file and sum the counts
-with open(input_file, mode='r', newline='') as file:
-    reader = csv.reader(file)
-    next(reader)  # Skip the header row
-    for row in reader:
-        category_name, count = row
-        total_images += int(count)
+annotation_path = '../annotations/instances_train2017.json'
+destination_path = '../json_coco_file/'
+spliting_file_name = ["T1_instances_train2017_split.json","T2_instances_train2017_split.json","T3_instances_train2017_split.json","T4_instances_train2017_split.json"]
+Class = [T1_COCO_CLASS_NAMES,T2_CLASS_NAMES,T3_CLASS_NAMES,T4_CLASS_NAMES]
 
-## idont know why 
-print(f"Total number of images: {total_images}")
+# ALl unspecified categories will change to 'unknown', and the number of images will be 15000 in default
+# If you want to mask all the categories, leave the Class empty
+mk.masking_json(annotation_path, destination_path, Class[0], num_images= 3000) 
