03bd5ef1d1eb37c2b828dea9693ae6b684cf084f
lethanhminh <21520063@gm.uit.edu.vn>
2024-06-25 05:38:00 +0700

    Create select.py using masked json file dataset

--- datasets/coco_split_file/masking.py
+++ datasets/coco_split_file/masking.py
@@ -2,20 +2,31 @@ import json
 import random 
 
 def masking_json(input_json, output_json, categories, num_images= 15000):
-    print("Running masking_json function")
     with open(input_json, 'r') as f:
         data = json.load(f)
 
     # Create a mapping from category id to category name
     category_name_to_id = {cat['name']: cat['id'] for cat in data['categories']}
     category_id_to_name = {cat['id']: cat['name'] for cat in data['categories']}
+    
+    print(f"Original categories: {category_name_to_id}")
+    print(f"Original categories: {category_id_to_name}")
 
     # Filter out category IDs based on the provided category names
     selected_category_ids = set(category_name_to_id[cat] for cat in categories if cat in category_name_to_id)
+    print(f"Selected category IDs: {selected_category_ids}")
 
-    # Randomly select the specified number of image ids
-    if len(image_ids) > num_images:
-        image_ids = set(random.sample(image_ids, num_images))
+    unknown_category_id = max(category_name_to_id.values()) + 1
+    new_categories = [{'id': category_name_to_id[cat], 'name': cat} for cat in categories if cat in category_name_to_id]
+    new_categories.append({'id': unknown_category_id, 'name': 'unknown'})
+
+    new_coco_dataset = {
+        'info': data.get('info', {}),
+        'licenses': data.get('licenses', []),
+        'images': [],
+        'annotations': [],
+        'categories': new_categories  # Add 'unknown' category
+    }
 
     # Track image ids that should be included
     image_ids = set()
@@ -25,27 +36,16 @@ def masking_json(input_json, output_json, categories, num_images= 15000):
         if ann['category_id'] in selected_category_ids:
             image_ids.add(ann['image_id'])
 
-    structure = {
-        'info': data.get('info', {}),
-        'licenses': data.get('licenses', []),
-        'images': [],
-        'annotations': [],
-        'categories': data['categories'] + [{'id': max(category_name_to_id.values()) + 1, 'name': 'unknown'}]  # Add 'unknown' category
-    }
-
-    unknown_category_id = max(category_name_to_id.values()) + 1
-
-
     # Filter annotations based on collected image ids and change unspecified categories to 'unknown'
     for ann in data['annotations']:
         if ann['image_id'] in image_ids:
             if ann['category_id'] not in selected_category_ids:
                 ann['category_id'] = unknown_category_id
-            structure['annotations'].append(ann)
+            new_coco_dataset['annotations'].append(ann)
 
     # Filter images based on collected image ids
-    structure['images'] = [img for img in data['images'] if img['id'] in image_ids]
+    new_coco_dataset['images'] = [img for img in data['images'] if img['id'] in image_ids]
 
     # Write the new JSON data to the output file
     with open(output_json, 'w') as f:
-        json.dump(structure, f, indent=4)
+        json.dump(new_coco_dataset, f, indent=4)
